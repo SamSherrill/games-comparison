99c985ca5f916d671f1a005e15223c2974a3e7dd
SamSherrill <38333695+SamSherrill@users.noreply.github.com>
2020-05-18 15:44:13 -0400

    Initial commit. Boilerplate MERN app setup.

---
+++ server.js
@@ -0,0 +1,29 @@
+require("dotenv").config();
+const express = require("express");
+const path = require("path");
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+app.use(express.urlencoded({
+    extended: true
+}));
+app.use(express.json());
+
+app.get("/api/config", (req, res) => {
+    res.json({
+        success: true,
+        currentPort: PORT,
+    });
+});
+
+app.use(express.static("client/build"));
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "/client/build/index.html"));
+});
+
+app.listen(PORT, () => {
+    console.log(`Express App is running on http://localhost:${PORT}`);
+});
