176da0d645ad5d62040e8cceb3ac28730685c994
SamSherrill <38333695+SamSherrill@users.noreply.github.com>
2020-07-30 16:02:30 -0400

    Added loading wheel that turns on & off. Added color variables to bassSCSS file and implemented them in all SCSS files. A few other small styling tweaks.

---
+++ client/src/components/LoadingWheel/LoadingWheel.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./LoadingWheel.scss";
+
+const LoadingWheel = () => {
+  return (
+    <>
+      <div className="loading-wheel"></div>
+    </>
+  );
+};
+
+export default LoadingWheel;
